737c3823b8e9c569fed3dfb3d742f3c99b29f141
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-03-21 14:29:13 +0200

    comment email verify

--- models/userModel.js
+++ models/userModel.js
@@ -1,45 +1,51 @@
-const crypto = require("crypto");
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "User must have a name"],
+    required: [true, 'User must have a name'],
   },
   email: {
     type: String,
-    required: [true, "User must have an email"],
+    required: [true, 'User must have an email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   role: {
     type: String,
-    enum: ["user", "employee", "admin"],
-    default: "user",
+    enum: ['blocked', 'user', 'employee', 'admin'],
+    default: 'user',
   },
   password: {
     type: String,
-    required: [true, "User must have a password"],
+    required: [true, 'User must have a password'],
     minlength: 8,
     select: false,
   },
   passwordConfirm: {
     type: String,
-    required: [true, "User must have a password confirmation"],
+    required: [true, 'User must have a password confirmation'],
     validate: {
       validator: function (el) {
         return el === this.password;
       },
-      message: "Passwords are not the same!",
+      message: 'Passwords are not the same!',
     },
+    // emailConfirmed:{
+    //   type: Boolean,
+    //   default: true,
+    // }
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  // emailVerifyToken: String,
+  // emailVerifyExpires: Date,
   active: {
     type: Boolean,
     default: true,
@@ -47,8 +53,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
 
@@ -56,12 +62,19 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.pre("save", function (next) {
-  if (!this.isModified("password") || this.isNew) return next();
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
+
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('email')) return next();
+
+//   this.emailConfirmed = false;
+//   next();
+// });
 
 userSchema.pre(/^find/, function (next) {
   this.find({ active: { $ne: false } });
@@ -88,18 +101,31 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 };
 
 userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
+  const resetToken = crypto.randomBytes(32).toString('hex');
 
   this.passwordResetToken = crypto
-    .createHash("sha256")
+    .createHash('sha256')
     .update(resetToken)
-    .digest("hex");
+    .digest('hex');
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
   return resetToken;
 };
 
-const User = mongoose.model("User", userSchema);
+// userSchema.methods.createEmailVerifyToken = function () {
+//   const verifyToken = crypto.randomBytes(32).toString('hex');
+
+//   this.emailVerifyToken = crypto
+//     .createHash('sha256')
+//     .update(verifyToken)
+//     .digest('hex');
+
+//   this.emailVerifyExpires = Date.now() + 10 * 60 * 1000;
+
+//   return verifyToken;
+// };
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
